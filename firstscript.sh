67b25ced3ae31c7bbfa43d1849218bb8f2b0caec
SashaVygodina <alexvygodina@gmail.com>
2025-07-01 21:48:05 +0000

    add iniitial script

---
+++ firstscript.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+# I'm outputing text with echo 
+echo "first script"
+
+# i'm listing the contents of my current directory
+ls
+
+# I'm using cd to move into my git directory
+cd ~/git/sasha/hello-world
+
+#  We're updating this repository
+git pull 
+
+# output the contents of README.md
+cat README.md
+
